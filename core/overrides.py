1c681c6fb54ab8cfe052689c6be2f344866b07d0
Autofire <Autofire@users.noreply.github.com>
2022-12-04 05:41:27 +0000

    Fixed insertion substitution.

--- core/overrides.py
+++ core/overrides.py
@@ -1,4 +1,5 @@
 from talon import Context, actions
+from .vim import Vim
 
 ctx = Context()
 @ctx.action_class("main")
@@ -6,7 +7,10 @@ class MainActions:
     def insert(text: str):
         #actions.key("i")
         # TODO: Silently switch to insert mode
+        changed = Vim.set_mode_without_notify_with("a")
         for char in text:
             actions.key(char)
         # TODO: Silently go back to old mode, if it makes sense
+        if changed:
+            Vim.set_mode_without_notify_with("esc")
 
--- core/vim.py
+++ core/vim.py
@@ -1,6 +1,7 @@
 from talon import Module, actions
 from enum import Enum
 from .events import Events
+from ..lib.delegate import Delegate
 
 mod = Module()
 mod.tag("wyrm_vim", desc="CodeWyrm currently has VIM features enabled")
@@ -17,40 +18,64 @@ class Vim:
         search = 4
         visual = 5
 
-        letterMap = {
-            command: ["escape", "esc"],
-            ex_command: [":"],
-            insert: ["i", "I", "a", "A", "o", "O", "R"],
-            search: ["/", "?"],
-            visual: ["v", "V", "ctrl-v"]
-        }
+    letterMap = {
+        Mode.command: ["escape", "esc"],
+        Mode.ex_command: [":"],
+        Mode.insert: ["i", "I", "a", "A", "o", "O", "R"],
+        Mode.search: ["/", "?"],
+        Mode.visual: ["v", "V", "ctrl-v"]
+    }
 
-        def parse(input: str):
-            map = Vim.Mode.letterMap
-            for mode in map.keys():
-                if input in map[mode]:
-                    return mode
+    _currentMode = Mode.command
 
-            return None
+    def parse(input: str):
+        map = Vim.letterMap
+        for mode in map.keys():
+            if input in map[mode]:
+                return mode
 
-
-    def set_mode_without_notify(newMode: Mode):
-        if(newMode is not None and Vim._currentMode != newMode):
-            Vim._currentMode = newMode
-
-    def set_mode(newMode: Mode):
-        if(newMode is not None and Vim._currentMode != newMode):
-            Vim._currentMode = newMode
-            Events.change_vim_mode.call(newMode)
-
-    def set_mode_without_notify_with(key: str):
-        Vim.set_mode_without_notify(Vim.Mode.parse(key))
-
-    def set_mode_with(key: str):
-        Vim.set_mode(Vim.Mode.parse(key))
+        return None
 
     def get_mode() -> Mode:
         return Vim._currentMode
+
+    def _enter_mode_with(key: str):
+        if Vim.get_mode() != Vim.Mode.command:
+            actions.key("esc")
+        actions.key(key)
+
+    def _can_set_mode(newMode: Mode) -> bool:
+        return newMode is not None and Vim.get_mode() != newMode
+
+    def set_mode_without_notify(newMode: Mode) -> bool:
+        result = Vim._can_set_mode(newMode)
+        if result:
+            Vim._currentMode = newMode
+        return result
+
+    def set_mode(newMode: Mode) -> bool:
+        result = Vim._can_set_mode(newMode)
+        if result:
+            Vim._currentMode = newMode
+            Events.change_vim_mode.call(newMode)
+        return result
+
+    def set_mode_without_notify_with(key: str) -> bool:
+        result = Vim.set_mode_without_notify(Vim.parse(key))
+        if result:
+            Vim._enter_mode_with(key)
+        return result
+
+    def set_mode_with(key: str) -> bool:
+        result = Vim.set_mode(Vim.parse(key))
+        if result:
+            Vim._enter_mode_with(key)
+        return result
+
+        
+
+
+
 
 
 
